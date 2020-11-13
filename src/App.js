8594d3a0cc445b430137e6a05dce12babe6db78e
DANIEL JAMES <daniel.james@codeimmersives.com>
2020-11-13 15:33:22 -0500

    added email format validator messaage to user 'Please enter your email address in desired format: yourname@example.com '

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import './App.css';
+import validator from 'validator';
 
 class App extends Component {
 
@@ -7,6 +8,8 @@ class App extends Component {
     state = {
       email: "",
       password: "",
+      effectualMessage: "",
+      isError: false,
     }
 
 //============================================================================================================//
@@ -16,12 +19,41 @@ handleOnChangeEmail = (event)=>{
 // set email state to whatever os updated in the email input
 this.setState({
   [event.target.name]: event.target.value,
+}, ()=>{
+  //grabbing the email in our state so we can work with it in verifying our inputs
+  const {email} = this.state;
+
+  // validator.isEmail to follow if inputted string becomes an accepted email format
+  // set to isEmail
+  let isEmail = validator.isEmail(email);
+
+  //  if isEmail results to true *- a validated email format 
+  //   do nothing, 
+  // else show alert to user to input an accepted email format
+  
+  if(isEmail){ 
+    this.setState({
+      isError: false,
+      effectualMessage: "",
+    })
+  }else {
+    console.log('email email; emal')
+    this.setState({
+      isError: true,
+      effectualMessage: `Please enter your email address in desired format: yourname@example.com  `
+    })
+  }
 } )}
 
 render () { 
+
+const {effectualMessage, isError}= this.state
+
   let showSignUpComponent = <form>
             <div className="login-info-box" >
             <h2>Sign Up</h2>
+            {isError ? <div>{effectualMessage}</div>: ""}
+            <br/>
                 <input 
                 type="text"
                 placeholder="enter email"
